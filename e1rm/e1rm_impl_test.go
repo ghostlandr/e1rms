cfea7a58d28fb9f3d7e5b65affda8307e168560d
Graham Holtslander <menello@gmail.com>
2022-07-28 09:22:28 -0600

    feat: add e1rm calc function

---
+++ e1rm/e1rm_impl_test.go
@@ -0,0 +1,39 @@
+package e1rm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_e1rms(t *testing.T) {
+	tests := []struct {
+		name        string
+		totalWeight float64
+		rpe         float64
+		reps        int16
+		want        float64
+	}{
+		{
+			name:        "CalculateE1RM should return zero if passed all zeroes",
+			totalWeight: 0,
+			rpe:         0,
+			reps:        0,
+			want:        0,
+		},
+		{
+			name:        "CalculateE1RM should return ~243 for 7 reps of 185 @ rpe 8",
+			totalWeight: 185,
+			rpe:         8,
+			reps:        7,
+			want:        243.42105263157896,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := CalculateE1RM(test.totalWeight, test.rpe, test.reps)
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
