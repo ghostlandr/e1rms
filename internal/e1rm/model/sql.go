24457b07a6e1081bec6f25e4a9a8b31e58da74d1
Graham H <8973794+ghostlandr@users.noreply.github.com>
2022-08-02 09:47:10 -0600

    fix: correct create table syntax

--- internal/e1rm/model/sql.go
+++ internal/e1rm/model/sql.go
@@ -1,6 +1,6 @@
 package e1rm_model
 
-var createTable = `CREATE TABLE e1rms IF NOT EXISTS (
+var createTable = `CREATE TABLE IF NOT EXISTS e1rms (
 	total_weight NUMERIC(5, 1)
 	rpe NUMERIC(5, 1)
 	reps integer
