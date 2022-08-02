c35db374ce5e7d2a7b0f36e971f08acd5c1eefc0
Graham H <8973794+ghostlandr@users.noreply.github.com>
2022-08-02 09:52:00 -0600

    fix: truly correct syntax this time

--- internal/e1rm/model/sql.go
+++ internal/e1rm/model/sql.go
@@ -1,8 +1,8 @@
 package e1rm_model
 
 var createTable = `CREATE TABLE IF NOT EXISTS e1rms (
-	total_weight NUMERIC(5, 1)
-	rpe NUMERIC(5, 1)
-	reps integer
+	total_weight NUMERIC(5, 1),
+	rpe NUMERIC(5, 1),
+	reps integer,
 	estimated_1rm NUMERIC(5, 1)
 )`
